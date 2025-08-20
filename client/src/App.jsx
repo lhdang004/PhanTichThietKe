4b9f5087f269b2b1f3e87021db784e7360078226
Le Dang <leedang2112@gmail.com>
2025-08-20 13:48:55 +0700

    creat home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { useLocation } from "react-router-dom";
+import { Route, Router, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
-  return <div>{!isOwnerPath && <Navbar />}</div>;
+  return (
+    <div>
+      {!isOwnerPath && <Navbar />}
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;
