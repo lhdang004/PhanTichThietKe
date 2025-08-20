ad67021564da5370a9517cc54d7ad6b606ea8c09
Le Dang <leedang2112@gmail.com>
2025-08-20 11:42:45 +0700

    create Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-
+import Navbar from "./components/Navbar";
+import { useLocation } from "react-router-dom";
 const App = () => {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
-  );
+  const isOwnerPath = useLocation().pathname.includes("owner");
+  return <div>{!isOwnerPath && <Navbar />}</div>;
 };
 
 export default App;
